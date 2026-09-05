d940aac00151a9f0816fc82d740ae5a48c79fcec
nnaatta <nmirovic002@gmail.com>
2026-09-05 15:13:21 +0200

    Izmena komponente ImageSlider

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -31,7 +31,7 @@ function ImageSlider({ slajdovi = [], visina = '460px', interval = 4000 }) {
 
       
       {trenutni.slika
-        ? <img src={trenutni.slika} className="img-slider__slika" loading="lazy" />
+        ? <img src={trenutni.slika} alt={trenutni.opis || ''} className="img-slider__slika" loading="lazy" />
         : <div className="img-slider__placeholder" />
       }
 
